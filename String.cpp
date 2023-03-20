3aa6043ded8e6f4f5a4fddaf0ba7ac1088807910
UTKars123 <65074702+UTKars123@users.noreply.github.com>
2023-03-20 23:47:06 +0530

    Add files via upload

--- String.cpp
+++ String.cpp
@@ -1,15 +1,33 @@
+//
+//#include <stdio.h>
+//#include<string.h>
+//#include<ctype.h>
+//int main()
+//{   char a[100];
+//    gets(a);
+//    int n=strlen(a);
+//    for(int i=0;i<n;i++){
+//        char c= tolower(a[i]);
+//        printf("%c",c);
+//		}
+//    
+//    return 0;
+//}
 
-#include <stdio.h>
-#include<string.h>
-#include<ctype.h>
-int main()
-{   char a[100];
-    gets(a);
-    int n=strlen(a);
-    for(int i=0;i<n;i++){
-        char c= tolower(a[i]);
-        printf("%c",c);
-		}
-    
-    return 0;
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num,rem,rev,sum=0;
+    cin>>num;
+    while(num!=0){
+	rem=num%10;
+	sum=10*sum+rem;
+    num=num/10;
+	}
+	cout<<sum;
+	  return 0;
 }
